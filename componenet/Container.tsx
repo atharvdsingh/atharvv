b91f1e313136379856ed33baa6b3b14678f105b9
atharvdsingh <dsingh9450492600@gmail.com>
2026-03-25 18:38:23 +0530

    init

---
+++ componenet/Container.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Container({children,className}:{children:React.ReactNode,className?:string}) {
+  return (
+    <div className={`max-w-5xl  mx-auto ${className}`} >{children}</div>
+  )
+}
